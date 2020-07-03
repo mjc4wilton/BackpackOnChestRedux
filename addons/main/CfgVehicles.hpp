72e4697c582a03a40ec96fe39ada0a0010a2f4d9
mjc4wilton <mjc4wilton@gmail.com>
2020-07-03 17:07:45 -0400

    switch icon path to macros

--- addons/main/CfgVehicles.hpp
+++ addons/main/CfgVehicles.hpp
@@ -15,21 +15,21 @@ class CfgVehicles
                          statement = QUOTE([_player] call FUNC(actionOnChest));
                          showDisabled = 0;
                          priority = 2.5;
-                         icon = "\zade_boc\data\actions\onchest_ca.paa";
+                         icon = QPATHOF(ui\onchest_ca.paa);
                     };
                     class BOCR_OnBack : BOCR_OnChest
                     {
                          displayName = CSTRING(onBack);
                          condition = QUOTE(!(GETMVAR(bocr_disabled, false)) && (backpack _player isEqualTo '') && !([_player] call FUNC(chestpack) isEqualTo ''));
                          statement = QUOTE([_player] call FUNC(actionOnBack));
-                         icon = "\zade_boc\data\actions\onback_ca.paa";
+                         icon = QPATHOF(ui\onback_ca.paa);
                     };
                     class BOCR_Swap : BOCR_OnChest
                     {
                          displayName = CSTRING(swap);
                          condition = QUOTE(!(GETMVAR(bocr_disabled, false)) && !(backpack _player isEqualTo '') && !([_player] call FUNC(chestpack) isEqualTo ''));
                          statement = QUOTE([_player] call FUNC(actionSwap));
-                         icon = "\zade_boc\data\actions\swap_ca.paa";
+                         icon = QPATHOF(ui\swap_ca.paa);
                     };
                };
           };
