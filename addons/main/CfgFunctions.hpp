6e9135288e4b3d403e0d719ebeb92e9a3da56e2f
mjc4wilton <mjc4wilton@gmail.com>
2020-07-14 21:18:18 -0400

    Add legacy warnings

---
+++ addons/main/CfgFunctions.hpp
@@ -0,0 +1,29 @@
+#define REDIRECT_PATH "\grad_boc\functions\zade_boc\fn_redirectFunction.sqf"
+
+class CfgFunctions {
+	class zade_boc {
+		tag = "zade_boc";
+		class zade_boc {
+			class actionOnBack{file=REDIRECT_PATH};
+			class actionOnChest{file=REDIRECT_PATH};
+			class actionSwap{file=REDIRECT_PATH};
+			class addChestpack{file=REDIRECT_PATH};
+			class addItemToChestpack{file=REDIRECT_PATH};
+			class addMagToChestpack{file=REDIRECT_PATH};
+			class canAddItemToChestpack{file=REDIRECT_PATH};
+			class chestpack{file=REDIRECT_PATH};
+			class chestpackContainer{file=REDIRECT_PATH};
+			class chestpackItems{file=REDIRECT_PATH};
+			class chestpackMagazines{file=REDIRECT_PATH};
+			class clearAllItemsFromChestpack{file=REDIRECT_PATH};
+			class loadChestpack{file=REDIRECT_PATH};
+			class removeChestpack{file=REDIRECT_PATH};
+			class removeItemFromChestpack{file=REDIRECT_PATH};
+			class removeMagFromChestpack{file=REDIRECT_PATH};
+
+			class moduleAdd{file=REDIRECT_PATH};
+			class moduleOnChest{file=REDIRECT_PATH};
+			class moduleDisable{file=REDIRECT_PATH};
+		};
+	};
+};
---
+++ addons/main/functions/fnc_legacyRedirect.sqf
@@ -0,0 +1,25 @@
+#include "script_component.hpp"
+/*
+ * Author: DerZade, mjc4wilton
+ * This function is for backwards compatibility. It redirects any of the
+ * old function with the prefix 'zade_boc' to the new 'bocr_main' function.
+ *
+ * Arguments:
+ * N/A
+ *
+ * Return Value:
+ * N/A
+ *
+ * Example:
+ * N/A
+ *
+ * Public: No
+ */
+if (_fnc_scriptName isEqualTo "") exitWith {};
+
+private _newFunction = format["bocr%1",_fnc_scriptName select [8]];
+
+private _warnMessage = format["'%1' is deprecated and no longer supported. Attempting to use '%2' instead. This may cause errors.", _fnc_scriptName, _newFunction];
+WARNING(_warnMessage);
+
+_this call (missionNamespace getVariable [_newFunction,{}]);
