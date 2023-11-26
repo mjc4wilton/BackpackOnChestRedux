8dc71ebbdd12bca9b3ee346acc55d3b3e0fb2726
johnb432 <58661205+johnb432@users.noreply.github.com>
2023-11-26 20:32:47 +0100

    ACE arsenal actions

--- addons/main/ACE_Arsenal_Actions.hpp
+++ addons/main/ACE_Arsenal_Actions.hpp
@@ -1,28 +1,27 @@
 class ace_arsenal_actions {
     class ADDON {
         displayName = CSTRING(ModuleOnChest_Displayname);
-        condition = QUOTE(_player call FUNC(canMovePack));
-        //scopeEditor = 0; // variables are reset between 3DEN and mission start
+        condition = QUOTE((_this select 0) call FUNC(canMovePack) && {backpack (_this select 0) != '' || {(_this select 0) call FUNC(chestpack) != ''}});
+        scopeEditor = 0; // editor modules exist for chestpacks
         tabs[] = {5};
-        /*
-        class GVAR(status) {
-            textStatement = QUOTE([_this select 0] call FUNC(weaponName));
+        class GVAR(chestpack) {
+            condition = QUOTE((_this select 0) call FUNC(chestpack) != '');
+            textStatement = QUOTE(FORMAT_1('%1',getText (configFile >> 'CfgVehicles' >> (_this select 0) call FUNC(chestpack) >> 'displayName')));
         };
-        */
         class GVAR(onChest) {
             label = CSTRING(OnChest);
-            condition = QUOTE(_player call FUNC(canMovePack) && {backpack _player != ''} && {_player call FUNC(chestpack) == ''});
-            statement = QUOTE(_player call FUNC(actionOnChest));
+            condition = QUOTE((_this select 0) call FUNC(canMovePack) && {backpack (_this select 0) != ''} && {(_this select 0) call FUNC(chestpack) == ''});
+            statement = QUOTE((_this select 0) call FUNC(actionOnChest));
         };
         class GVAR(onBack) {
             label = CSTRING(OnBack);
-            condition = QUOTE(_player call FUNC(canMovePack) && {backpack _player == ''} && {_player call FUNC(chestpack) != ''});
-            statement = QUOTE(_player call FUNC(actionOnBack));
+            condition = QUOTE((_this select 0) call FUNC(canMovePack) && {backpack (_this select 0) == ''} && {(_this select 0) call FUNC(chestpack) != ''});
+            statement = QUOTE((_this select 0) call FUNC(actionOnBack));
         };
         class GVAR(swap) {
             label = CSTRING(Swap);
-            condition = QUOTE(_player call FUNC(canMovePack) && {backpack _player != ''} && {_player call FUNC(chestpack) != ''});
-            statement = QUOTE(_player call FUNC(actionSwap));
+            condition = QUOTE((_this select 0) call FUNC(canMovePack) && {backpack (_this select 0) != ''} && {(_this select 0) call FUNC(chestpack) != ''});
+            statement = QUOTE((_this select 0) call FUNC(actionSwap));
         };
     };
 };
