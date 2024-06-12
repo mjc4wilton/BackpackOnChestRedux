aadc3081828a9512561687dbfac926650872cfc4
mjc4wilton <mjc4wilton@gmail.com>
2024-06-12 17:24:42 -0400

    Main - Bump required version to Arma 1.58
    Fixes HEMTT errors caused by event handler implementation versions

--- addons/main/script_mod.hpp
+++ addons/main/script_mod.hpp
@@ -9,7 +9,7 @@
 #define VERSION_AR MAJOR,MINOR,PATCHLVL,BUILD
 
 // MINIMAL required version for the Mod. Components can specify others..
-#define REQUIRED_VERSION 1.56
+#define REQUIRED_VERSION 1.58
 
 #ifdef COMPONENT_BEAUTIFIED
     #define COMPONENT_NAME QUOTE(BOCR - COMPONENT_BEAUTIFIED)
